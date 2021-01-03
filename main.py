4f24951eb00c3c91ab8f98375e64de65c5bad89f
YAN GURENKO <73452308+Qcalibur@users.noreply.github.com>
2021-01-04 00:33:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ root.title('Text Editor')
 root.geometry('500x650')
 root.resizable(width=False, height=False)
 
-text = scrolledtext.ScrolledText(root, width=60, height=40)
-text.grid(column=0, row=0)
+entry_field = scrolledtext.ScrolledText(root, width=60, height=40)
+entry_field.grid(column=0, row=0)
 
 root.mainloop()
