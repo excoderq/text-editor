a7ce9fd518df824ebe4c0665dd6b1f645ad4343e
YAN GURENKO <73452308+Qcalibur@users.noreply.github.com>
2021-03-07 20:38:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+# импорт библиотеки tkinter
 import tkinter
 from tkinter import scrolledtext
 
+# размер приложения
 root = tkinter.Tk()
 root.title('Text Editor')
 root.geometry('500x650')
+# удаление возможности развёртывания во весь экран
 root.resizable(width=False, height=False)
 
+# создание поля для текста
 entry_field = scrolledtext.ScrolledText(root, width=60, height=40)
 entry_field.grid(column=0, row=0)
 
